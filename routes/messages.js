7290507adacc9a8989f276ffd437fbcfb170fe71
IgorTikush <igortikush@mail.ru>
2021-05-10 14:34:30 +1000

    first commit

---
+++ routes/messages.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('messages', { title: 'Messages', messages: messages });
+  
+});
+
+router.get('/new', (req,res, next) => {
+  res.render('form');
+})
+
+//create new message
+router.post('/new', (req, res, next) => {
+    messages.push({ text: req.body.message, user: req.body.name, added: new Date()})
+    res.redirect('/messages');
+})
+
+module.exports = router;
